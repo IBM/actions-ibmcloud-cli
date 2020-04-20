9961504687ce062ab534ce21ce5f52d1adcd50f0
JJ Asghar <jjasghar@gmail.com>
2020-04-20 14:57:00 -0500

    Removed the container-registry
    Signed-off-by: JJ Asghar <awesome@ibm.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,4 @@ curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
 ibmcloud --version
 ibmcloud config --check-version=false
 ibmcloud plugin install -f kubernetes-service
-ibmcloud plugin install -f container-registry
 ibmcloud login --apikey "$1" -r "$2" -g default
-ibmcloud cr region-set "$2"
-ibmcloud cr login

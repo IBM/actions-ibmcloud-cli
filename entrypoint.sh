95327c0d9d83450fdf84355bb3318d5bccc2a4f9
Steve Martinelli <4118756+stevemar@users.noreply.github.com>
2020-04-22 14:11:43 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 #curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
-curl -sL https://ibm.biz/idt-installer | sh
+curl -sL https://ibm.biz/idt-installer | bash
 ibmcloud --version
 ibmcloud config --check-version=false
 ibmcloud login --apikey "$1" -r "$2" -g default

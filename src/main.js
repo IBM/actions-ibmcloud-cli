5506a9794633632842da5ecd4b2d38e1ee9b555f
Jason Frey <fryguy9@gmail.com>
2024-06-13 18:36:09 -0400

    Add plugins input
    Signed-off-by: Jason Frey <fryguy9@gmail.com>

--- src/main.js
+++ src/main.js
@@ -19,6 +19,13 @@ async function run() {
     });
     version = version.split('+')[0].split(' ')[2]
     core.setOutput('version', version)
+
+    // Install plugins
+    const plugins = core.getInput('plugins').replace('\n', ' ').replace(',', ' ').split(' ').map(p => p.trim()).filter(p => p)
+    if (plugins.length > 0) {
+      await exec.exec('ibmcloud', ['plugin', 'install', ...plugins])
+      await exec.exec('ibmcloud', ['plugin', 'list'])
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
